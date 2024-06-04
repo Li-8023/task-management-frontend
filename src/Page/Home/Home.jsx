f94d1cac1a4cd7a432621e8a21a05868159fd736
Li He <80284104+Li-8023@users.noreply.github.com>
2024-06-04 10:30:06 +0800

    add the expand menu

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -4,15 +4,15 @@ import TaskList from '../TaskList/TaskList'
 
 function Home() {
   return (
-    <div className='lg:flex px-5 lg:px-20 pt-[2.9vh]'>
-        <div className='hidden lg:block w-[25vw] relative mr-4'>
-            <Sidebar />
-        </div>
-        <div className='right-side-part w-full flex justify-center mb-10 ml-5'>
-          <TaskList />
-        </div>
+    <div className="lg:flex px-5 lg:px-16 pt-[2.9vh]">
+      <div className="hidden lg:block w-[25vw] relative mr-2">
+        <Sidebar />
+      </div>
+      <div className="right-side-part w-full flex justify-center mb-10 ml-10">
+        <TaskList />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
--- src/Page/Task/TaskCard/TaskCard.jsx
+++ src/Page/Task/TaskCard/TaskCard.jsx
@@ -1,10 +1,52 @@
-import { IconButton } from '@mui/material';
-import React from 'react'
+import { IconButton } from "@mui/material";
+import React, { useState } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import UserList from "../UserList";
+import SubmissionList from "../SubmissionList";
 
+const role="ROLE_ADMIN";
 
+function TaskCard(){
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const openMenu = Boolean(anchorEl);
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
-function TaskCard() {
+  const [openUserList, setOpenUserList] = useState(false);
+
+  const handleOpenUserList = () =>{
+    setOpenUserList(true);
+    handleMenuClose();
+  };
+
+  const handleCloseUserList = () =>{
+    setOpenUserList(false);
+  }
+
+  const [openSubmissionList, setOpenSubmissionList] = useState(false);
+
+  const handleOpenSubmissionList = () => {
+    setOpenSubmissionList(true);
+    handleMenuClose();
+  };
+
+  const handleCloseSubmissionList = () => {
+    setOpenSubmissionList(false);
+  };
+  const handleOpenUpdateTaskModel = () => {
+
+  };
+
+  const handleDeleteTask = () => {
+    
+  };
+
   return (
     <div>
       <div className="card lg:flex justify-between">
@@ -33,13 +75,47 @@ function TaskCard() {
           </div>
         </div>
         <div>
-            <IconButton>
-                <MoreVertIcon />
-            </IconButton>
+          <IconButton
+            id="basic-button"
+            aria-controls={openMenu ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={openMenu ? "true" : undefined}
+            onClick={handleMenuClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={openMenu}
+            onClose={handleMenuClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            {role === "ROLE_ADMIN" ? (
+              <>
+                <MenuItem onClick={handleOpenUserList}>Assigned User</MenuItem>
+                <MenuItem onClick={handleOpenSubmissionList}>
+                  See Submission
+                </MenuItem>
+                <MenuItem onClick={handleOpenUpdateTaskModel}>Edit</MenuItem>
+                <MenuItem onClick={handleDeleteTask}>Delete</MenuItem>
+              </>
+            ) : (
+              <></>
+            )}
+          </Menu>
         </div>
       </div>
+      {openUserList && (
+        <UserList open={openUserList} handleClose={handleCloseUserList} />
+      )}
+      {openSubmissionList && (
+        <SubmissionList open={openSubmissionList} handleClose={handleCloseSubmissionList} />
+      )}
     </div>
   );
 }
 
-export default TaskCard
+export default TaskCard;
